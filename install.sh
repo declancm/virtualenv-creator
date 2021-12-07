116de1edff4e27f858af506cdebcfa0246a9ba4a
declancm <declanmullen3.14@gmail.com>
2021-12-07 14:09:04 -0800

    Improved logic in install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
-[ ! -d ~/pyvenv-creator ] && mkdir -p ~/pyvenv-creator && printf "\nThe pyvenv-creator directory was created.\n"
+[ ! -d ~/pyvenv-creator ] && mkdir -p ~/pyvenv-creator && printf "The pyvenv-creator directory was created.\n"
 aliasSource="source $HOME/pyvenv-creator/pyvenv.sh"
 cp -f ./pyvenv.sh ~/pyvenv-creator/pyvenv.sh
-cp -f ./pyvenv-alias ~/pyvenv-creator/pyvenv-alias
-printf "\nWould you like to source the alias for bash? (y/n) "
+[ -d ~/pyvenv-creator/pyvenv-alias ] && rm -f ~/pyvenv-creator/pyvenv-alias
+printf "alias pyvenv=\"source $HOME/pyvenv-creator/pyvenv.sh\"" >> ~/pyvenv-creator/pyvenv-alias
+printf "Would you like to source the alias for bash? (y/n) "
 read input
 if [ "$input" = "y" ] || [ "$input" = "Y" ] || [ "$input" = "yes" ] || [ "$input" = "Yes" ]
 then
     if grep -qF "$aliasSource" ~/.bashrc;then
-        printf "\nThe source link already exists.\n\n"
+        printf "The source link already exists.\n"
     else
         printf "\n$aliasSource\n" >> ~/.bashrc
-        printf "\nThe alias was sourced successfully.\n\n"
+        printf "The alias was sourced successfully.\n"
     fi
 else
-    printf "\nThe alias was not added.\n\n"
+    printf "The alias was not added.\n"
 fi
