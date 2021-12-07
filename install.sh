4f10091ed5c5461e373296a290a0c297d1a1e785
declancm <declanmullen3.14@gmail.com>
2021-12-06 17:05:11 -0800

    Fixed messages in install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 newAlias="alias pyvenv='source ~/pyvenv-creator/pyvenv.sh"
-[ ! -d ~/pyvenv-creator ] && mkdir -p ~/pyvenv-creator && printf "\nThe pyvenv-creator directory was created.\n\n"
+[ ! -d ~/pyvenv-creator ] && mkdir -p ~/pyvenv-creator && printf "\nThe pyvenv-creator directory was created.\n"
 cp -f ./pyvenv.sh ~/pyvenv-creator/pyvenv.sh
-printf "Would you like to install the alias for bash or zsh? "
+printf "\nWould you like to install the alias for bash or zsh? "
 read input
 if [ "$input" = "bash" ] || [ "$input" = "Bash" ]
 then
@@ -13,10 +13,11 @@ then
 elif [ "$input" = "zsh" ] || [ "$input" = "Zsh" ]
 then
     if grep -qF "$newAlias" ~/.zshrc;then
-        printf "\nThe alias already exists.\n"
+        printf "\nThe alias already exists.\n\n"
     else
         printf "$newAlias" >> ~/.zshrc
+        printf "\nThe alias was added successfully.\n\n"
     fi
 else
-    printf "\nYou did not enter a valid option. Neither were created.\n"
+    printf "\nYou did not enter a valid option. Neither were created.\n\n"
 fi
