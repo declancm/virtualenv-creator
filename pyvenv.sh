d875473011a3d94a75f065acf9fc3adeace0670f
declancm <declanmullen3.14@gmail.com>
2021-12-10 11:57:42 -0800

    Activating instruction now provides full path.

--- pyvenv.sh
+++ pyvenv.sh
@@ -7,8 +7,10 @@ read directory
 directory="${directory/#\~/$HOME}"
 printf "Enter the name of the python virtualenv: "
 read name
-if [ ! -d "$directory/$name" ]
+if [ -d "$directory/$name" ]
 then
+    printf "A folder already exists in that directory with that name.\n"
+else
     printf "\nThe installed python versions:\n\n"
     ls -ls /usr/bin/python*[^config]
     printf "\nEnter the version of python you would like to use (eg. 3.9): "
@@ -18,34 +20,42 @@ then
     virtualenv --python python$version $directory/$name >/dev/null
     if [ $? -eq 0 ]
     then
-        printf "Would you like to install pandas and matplotlib? (y/n) "
-        read libraries
-        if [ "$libraries" = "y" ] || [ "$libraries" = "Y" ] || [ "$libraries" = "yes" ] || [ "$libraries" = "Yes" ]
-        then
-            source $directory\/$name/bin/activate
-            if [ $? -eq 0 ]
+        while :
+        do
+            printf "Enter the name of a library you would like to install (select enter to skip) : "
+            read libraries
+            if [ "$libraries" != "" ]
             then
-                if [ "${version:0:1}" == "3" ]
+                source $directory\/$name/bin/activate
+                if [ $? -eq 0 ]
                 then
-                    pip3 install pandas
-                    pip3 install matplotlib
-                    #pip3 install sklearn
-                elif [ "${version:0:1}" == "2" ]
-                then
-                    pip2 install pandas
-                    pip2 install matplotlib
-                    #pip2 install sklearn
+                    if [ "${version:0:1}" = "3" ]
+                    then
+                        pip3 install $libraries
+                        if [ $? -eq 0 ]
+                        then
+                            printf "\nThe library installation was successfull.\n\n"
+                        else
+                            printf "\nThe library installation was unsuccessful.\n\n"
+                        fi
+                    elif [ "${version:0:1}" = "2" ]
+                    then
+                        pip install $libraries
+                        if [ $? -eq 0 ]
+                        then
+                            printf "\nThe library installation was successfull.\n\n"
+                        else
+                            printf "\nThe library installation was unsuccessful.\n\n"
+                        fi
+                    fi
+                    deactivate
+                else
+                    printf "The python virtualenv could not be activated."
                 fi
-                deactivate
             else
-                printf "The python virtualenv could not be activated."
+                break
             fi
-        elif [ "$libraries" = "n" ] || [ "$libraries" = "N" ] || [ "$libraries" = "no" ] || [ "$libraries" = "No" ]
-        then
-            printf "Libraries will not be installed.\n"
-        else
-            printf "You did not enter a valid answer. Libraries will not be installed.\n"
-        fi
+        done
         printf "\nDo you want to activate the python venv? (y/n) "
         read activate
         if [ "$activate" = "y" ] || [ "$activate" = "Y" ] || [ "$activate" = "yes" ] || [ "$activate" = "Yes" ]
@@ -54,16 +64,20 @@ then
         elif [ "$activate" = "n" ] || [ "$activate" = "N" ] || [ "$activate" = "no" ] || [ "$activate" = "No" ]
         then
             printf "The python virtualenv will not be activated.\n"
-            printf "To activate: source $directory/$name/bin/activate\n"
         else
             printf "You did not enter a valid answer. The python virtualenv will not be activated.\n"
-            printf "To activate: source $directory/$name/bin/activate\n"
         fi
-        printf "\nThe python virtualenv is created.\n\n"
+        if [ "$directory" = "." ]
+        then
+            directory=$(pwd)
+        elif [ "${directory:0:2}" = "./" ]
+        then
+            cd $directory
+            directory=$(pwd)
+            cd -
+        fi
+        printf "\nThe python virtualenv is created.\nTo manually activate (from within any directory): source $directory/$name/bin/activate\n\n"
     else
         printf "\nThe python virtualenv could not be created.\n"
     fi
-    # cd -
-else
-    printf "A folder already exists in that directory with that name.\n"
 fi
