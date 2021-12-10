932ba2828d16555e54df1ede61c1c6a158daed84
declancm <declanmullen3.14@gmail.com>
2021-12-10 01:08:44 -0800

    auto commit @ 2021-12-10 01:08:44

--- pyvenv.ps1
+++ pyvenv.ps1
@@ -1,10 +1,10 @@
-$directoryString = Read-Host -Prompt "`nEnter the directory path where the python virtual environment will be installed"
+$directoryString = Read-Host -Prompt "`nEnter the directory path where the python virtualenv will be installed"
 $directory = Get-Item $directoryString
-$name = Read-Host -Prompt "Enter the name of the python virtual environment"
+$name = Read-Host -Prompt "Enter the name of the python virtualenv"
 if(Test-Path -Path "$directory\$name" -PathType Container) {
   "A folder already exists in that directory with that name.`n"
 } else {
-  "`nThe python virtual environment is being created...`n"
+  "`nThe python virtualenv is being created...`n"
   Invoke-Expression "py -m venv $directory\$name"
   if($?) {
     $libraries = Read-Host -Prompt "Do you want to install pandas and matplotlib? (y/n) "
@@ -19,19 +19,19 @@ if(Test-Path -Path "$directory\$name" -PathType Container) {
       "You did not enter a valid answer. Libraries will not be installed.`n"
     }
     # "`nTo activate the python venv: $directory\$name\Scripts\activate.ps1`n"
-    $activate = Read-Host -Prompt "Do you want to activate the python venv? (y/n) "
+    $activate = Read-Host -Prompt "Do you want to activate the python virtualenv? (y/n) "
     if ($activate -eq 'y') {
       Invoke-Expression "$directory\$name\Scripts\activate.ps1"
     } elseif ($activate -eq 'n') {
-      "The python venv will not be activated.`n"
-      "To activate the python venv: $directory\$name\Scripts\activate.ps1"
+      "The python virtualenv will not be activated.`n"
+      "To activate the python virtualenv: $directory\$name\Scripts\activate.ps1"
     }
     else {
-      "You did not enter a valid answer. The python venv will not be activated.`n"
-      "To activate the python venv: $directory\$name\Scripts\activate.ps1"
+      "You did not enter a valid answer. The python virtualenv will not be activated.`n"
+      "To activate the python virtualenv: $directory\$name\Scripts\activate.ps1"
     }
-    "`nThe python venv creation is complete.`n"
+    "`nThe python virtualenv is created.`n"
   } else {
-    "`nThe python virtual environment could not be created.`n"
+    "`nThe python virtualenv could not be created.`n"
   }
 }
