870440ac675e92ab5abee1e101db27ca285e5a80
declancm <declanmullen3.14@gmail.com>
2021-12-10 15:14:03 -0800

    Allowed python.exe selection.

--- pyvenv.ps1
+++ pyvenv.ps1
@@ -1,37 +1,44 @@
-$directoryString = Read-Host -Prompt "`nEnter the directory path where the python virtualenv will be installed"
+$directoryString = Read-Host -Prompt "`nEnter the directory path where the python virtual environment will be installed"
 $directory = Get-Item $directoryString
-$name = Read-Host -Prompt "Enter the name of the python virtualenv"
+$name = Read-Host -Prompt "Enter the name of the python virtual environment"
 if(Test-Path -Path "$directory\$name" -PathType Container) {
   "A folder already exists in that directory with that name.`n"
 } else {
-  "`nThe python virtualenv is being created...`n"
-  Invoke-Expression "py -m venv $directory\$name"
+  $where = where.exe Python
+  $number = 0
+  "`nThe installed python.exe versions:`n"
+  while ( $where[$number] ) {
+    $current = $where[$number]
+    "    $number.   $current"
+    $number = $number + 1
+  }
+  $version = Read-Host -Prompt "`nEnter the list number of the python.exe you would like to use"
+  $python = $where[$version]
+  "`nThe python virtual environment is being created...`n"
+  Invoke-Expression "virtualenv --python $python $directory\$name"
   if($?) {
-    $libraries = Read-Host -Prompt "Do you want to install pandas and matplotlib? (y/n) "
-    if($libraries -eq 'y') {
-      Invoke-Expression "$directory\$name\Scripts\activate.ps1"
-      Invoke-Expression 'py -m pip install pandas'
-      Invoke-Expression 'py -m pip install matplotlib'
-      Invoke-Expression 'deactivate'
-    } elseif($libraries -eq 'n') {
-      "Libraries will not be installed.`n"
-    } else {
-      "You did not enter a valid answer. Libraries will not be installed.`n"
+    while($true) {
+      $libraries = Read-Host -Prompt "`nEnter the name of a library you would like to install (press Enter to skip)"
+      if($libraries -ne '') {
+        Invoke-Expression "$directory\$name\Scripts\activate.ps1"
+        Invoke-Expression "py -m pip install $libraries"
+        Invoke-Expression 'deactivate'
+      }
+      else {
+        break
+      }
     }
-    # "`nTo activate the python venv: $directory\$name\Scripts\activate.ps1`n"
-    $activate = Read-Host -Prompt "Do you want to activate the python virtualenv? (y/n) "
+    $activate = Read-Host -Prompt "`nDo you want to activate the python venv? (y/n) "
     if ($activate -eq 'y') {
       Invoke-Expression "$directory\$name\Scripts\activate.ps1"
     } elseif ($activate -eq 'n') {
-      "The python virtualenv will not be activated.`n"
-      "To activate the python virtualenv: $directory\$name\Scripts\activate.ps1"
+      "The python venv will not be activated."
     }
     else {
-      "You did not enter a valid answer. The python virtualenv will not be activated.`n"
-      "To activate the python virtualenv: $directory\$name\Scripts\activate.ps1"
+      "You did not enter a valid answer. The python venv will not be activated."
     }
-    "`nThe python virtualenv is created.`n"
+    "`nThe python virtualenv creation is complete.`n`nTo activate the python virtualenv: $directory\$name\Scripts\activate.ps1`n"
   } else {
-    "`nThe python virtualenv could not be created.`n"
+    "`nThe python virtual environment could not be created.`n"
   }
 }
