353b216ea7196d5adef4d6e0b0719cd40102b8b4
declancm <declanmullen3.14@gmail.com>
2021-12-11 13:48:58 -0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,8 +2,6 @@ $setAlias = Read-Host -Prompt "`nWould you like to add an alias to PowerShell? (
 if ($setAlias -eq 'y') {
   $location = Write-Output $profile
   $alias = [Regex]::Escape("function runPyvenv { Invoke-Expression `". ~\Documents\virtualenv-creator\pyvenv.ps1`" }`nSet-Alias pyvenv runPyvenv")
-  #$alias = Select-String -Path $location -Pattern [Regex]::Escape("function runPyvenv { Invoke-Expression `". ~\Documents\virtualenv-creator\pyvenv.ps1`" }`nSet-Alias pyvenv runPyvenv")
-  #if ($null -ne $alias) {
   if (Get-Content $location | ForEach-Object{$_ -match $alias}) {
     "The alias has already been added to PowerShell.`n"
   }
