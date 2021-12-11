6c22d44d2df268cfc6cf694abde7cffcc15c07a3
declancm <declanmullen3.14@gmail.com>
2021-12-11 08:42:48 -0800

    Added echo $profile.

--- install.ps1
+++ install.ps1
@@ -1,9 +1,10 @@
-$alias = Select-String -Path ~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 -Pattern "function runPyvenv { Invoke-Expression `". ~\Documents\virtualenv-creator\pyvenv.ps1`" }`nSet-Alias pyvenv runPyvenv"
+$location = Invoke-Expression "echo $profile"
+$alias = Select-String -Path $location -Pattern "function runPyvenv { Invoke-Expression `". ~\Documents\virtualenv-creator\pyvenv.ps1`" }`nSet-Alias pyvenv runPyvenv"
 if ($alias -ne $null)
 {
     "The alias has already been added to PowerShell."
 }
 else
 {
-    Add-Content ~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 "`nfunction runPyvenv { Invoke-Expression `". ~\Documents\virtualenv-creator\pyvenv.ps1`" }`nSet-Alias pyvenv runPyvenv"
+    Add-Content $location "`nfunction runPyvenv { Invoke-Expression `". ~\Documents\virtualenv-creator\pyvenv.ps1`" }`nSet-Alias pyvenv runPyvenv"
 }
