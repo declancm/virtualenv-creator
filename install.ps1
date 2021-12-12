909f793408c962311bbfad0cd0299f9f50c301f4
declancm <declanmullen3.14@gmail.com>
2021-12-11 18:00:27 -0800

    Fixed string search.

--- install.ps1
+++ install.ps1
@@ -1,12 +1,11 @@
 $setAlias = Read-Host -Prompt "`nWould you like to add an alias to PowerShell? (y/n)"
 if ($setAlias -eq 'y') {
   $location = Write-Output $profile
-  $alias = "`nfunction runPyvenv { Invoke-Expression `". ~\Documents\virtualenv-creator\pyvenv.ps1`" }`nSet-Alias pyvenv runPyvenv"
-  if (Select-String -Path $location -Pattern $alias -SimpleMatch -Quiet) {
+  if (Select-String -Path $location -Pattern "Set-Alias pyvenv runPyvenv" -SimpleMatch -Quiet) {
     "The alias has already been added to PowerShell.`n"
   }
   else {
-    Add-Content $location "`n$alias"
+    Add-Content $location "`nfunction runPyvenv { Invoke-Expression `". ~\Documents\virtualenv-creator\pyvenv.ps1`" }`nSet-Alias pyvenv runPyvenv"
     "The alias was added.`n"
   }
 }
