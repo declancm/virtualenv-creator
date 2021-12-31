4573916bde6af1e7811b01d1d659761afafab843
declancm <declanmullen3.14@gmail.com>
2021-12-31 00:23:42 -0800

    auto commit @ 2021-12-31 08:23:42 UTC

--- src/pyvenv-PS.ps1
+++ src/pyvenv-PS.ps1
@@ -37,11 +37,12 @@ if($initialInput -eq 'c') {
     "`nThe python virtual environment is being created..."
     $status = Invoke-Expression "virtualenv --python $python $directory\$name"
     if($status) {
-      if($Null -eq (Get-Content -Path $virtualenvList)) {
-        Add-Content -Path $virtualenvList -Value "$directory\$name" -Force
-      } else {
-        Add-Content -Path $virtualenvList -Value "`n$directory\$name" -Force
-      }
+      # if($Null -eq (Get-Content -Path $virtualenvList)) {
+      #   Add-Content -Path $virtualenvList -Value "$directory\$name" -Force
+      # } else {
+      #   Add-Content -Path $virtualenvList -Value "`n$directory\$name" -Force
+      # }
+      Add-Content -Path $virtualenvList -Value "$directory\$name" -Force
       while($true) {
         $libraries = Read-Host -Prompt "`nEnter the name of a library you would like to install (press Enter to skip) "
         if($libraries -ne '') {
