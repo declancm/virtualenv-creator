921cf620c841c907e66af66a9c79ad6ad1bca568
declancm <declanmullen3.14@gmail.com>
2021-12-31 20:17:24 -0800

    Fixed PowerShell list.

--- src/pyvenv-PS.ps1
+++ src/pyvenv-PS.ps1
@@ -83,18 +83,19 @@ if($initialInput -eq 'c') {
     if(Test-Path $virtualenvList) {
       if($Null -eq (Get-Content -Path $virtualenvList)) {
         "`nThe list file is empty. Have you created a virtualenv?"
-        Return
+        ""; Return
       }
     } else {
       "`nA list file does not exit. Have you created a virtualenv?"
-        Return
+        ""; Return
     }
     [string[]]$list = Get-Content -Path $virtualenvList
-    $n = $list.Length
+    $n = $list.Length - 1
     $currentLine = $list[$n]
     while($Null -ne $currentLine) {
       if(-Not (Test-Path $currentLine\Scripts\activate.ps1)) {
         # Set-Content -Path $virtualenvList -Value (Get-Content -Path $virtualenvList | Select-String -Pattern $currentLine)
+        $currentLine = [regex]::Escape($currentLine)
         Set-Content -Path $virtualenvList -Value (Get-Content -Path $virtualenvList | Select-String -Pattern $currentLine -NotMatch)
       }
       $script:n--
@@ -102,21 +103,23 @@ if($initialInput -eq 'c') {
     }
     if($Null -eq (Get-Content -Path $virtualenvList)) {
       "`nThe list file is empty. Have you created a virtualenv?"
-      Return
+      ""; Return
     }
     [string[]]$list = Get-Content -Path $virtualenvList
     "`nA list of your created python virtualenvs :`n"
     $n = 0
+    $nPlus = $n + 1
     $currentLine = $list[$n]
     while($Null -ne $currentLine) {
-      "    $n.   $currentLine"
+      "    $nPlus.   $currentLine"
       $script:n++
+      $script:nPlus++
       $currentLine = $list[$n]
     }
     $inputNumber = Read-Host -Prompt "`nTo select a virtualenv enter the list number (or press Enter to exit) "
+    if ($inputNumber -eq '') { ""; Return }
     $number = [int]$inputNumber
-    $selectedLine = $list[$number]
-    if($number -eq '') { Return }
+    $selectedLine = $list[$number - 1]
     $userInput = Read-Host -Prompt "`nEnter 'a' to activate or 'd' to delete '$selectedLine' (or press Enter to cancel) "
     if($userInput -eq 'a') {
       Invoke-Expression "$selectedLine\Scripts\activate.ps1"
@@ -135,8 +138,8 @@ if($initialInput -eq 'c') {
     else { "You did not enter a valid option." }
   }
 } elseif($initialInput -eq '') {
-  Return
+  ""; Return
 } else {
   "`nThat was not a valid input.`n"
-  Return
+  ""; Return
 }
